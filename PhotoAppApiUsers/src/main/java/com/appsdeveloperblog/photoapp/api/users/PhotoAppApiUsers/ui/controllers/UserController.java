b6555c1917f079f69496590dc378542ef0d37011
Kolotushka1 <maksim.kolotov.050605@mail.ru>
2024-04-08 12:22:33 +0700

    Initial commit

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsers/ui/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.photoapp.api.users.PhotoAppApiUsers.ui.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Environment env;
+
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+
+}
